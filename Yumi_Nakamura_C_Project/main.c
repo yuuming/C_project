17219aa3ea6365e58fd01cb3f2281a2a3281e2b7
Yuumi <yuuming.n@gmail.com>
2017-11-15 15:09:29 -0800

    Initial Commit

---
+++ Yumi_Nakamura_C_Project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Yumi_Nakamura_C_Project
+//
+//  Created by Yumi Nakamura on 2017-11-15.
+//  Copyright © 2017 Yumi Nakamura. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
